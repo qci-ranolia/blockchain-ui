ea167f10cae1dde940957989eeb64d82a4633a95
deepak-ranolia <d.ranolia92@gmail.com>
2018-12-13 02:42:07 +0530

    sidebar design check with nebular theme destroyed in previous commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    $(document).ready(function () {
+      var trigger = $('.hamburger'),
+          overlay = $('.overlay'),
+        isClosed = false;
+      trigger.click(function () {
+        hamburger_cross();      
+      });
+    
+      function hamburger_cross() {
+        if (isClosed == true) {          
+          overlay.hide();
+          trigger.removeClass('is-open');
+          trigger.addClass('is-closed');
+          isClosed = false;
+        } else {   
+          overlay.show();
+          trigger.removeClass('is-closed');
+          trigger.addClass('is-open');
+          isClosed = true;
+        }
+      }  
+      $('[data-toggle="offcanvas"]').click(function () {
+            $('#wrapper').toggleClass('toggled');
+      });  
+    });
+  }
 
   ngOnInit() {
   }
-
 }
