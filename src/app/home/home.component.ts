18aad12ff93bc3f86c1a0c35c9f31eb4619640bd
deepak-ranolia <d.ranolia92@gmail.com>
2018-12-13 15:07:20 +0530

    data tables added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 declare var $
 
 @Component({
@@ -6,34 +6,36 @@ declare var $
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
 
+export class HomeComponent implements OnInit {
   constructor() {
     $(document).ready(function () {
+      $('#table_id').DataTable({
+        scrollY: 380
+      })
       var trigger = $('.hamburger'),
           overlay = $('.overlay'),
-        isClosed = false;
+          isClosed = false
       trigger.click(function () {
-        hamburger_cross();      
-      });
-    
+        hamburger_cross()      
+      })
       function hamburger_cross() {
-        if (isClosed == true) {          
-          overlay.hide();
-          trigger.removeClass('is-open');
-          trigger.addClass('is-closed');
-          isClosed = false;
+        if (isClosed == false) {          
+          overlay.hide()
+          trigger.removeClass('is-open')
+          trigger.addClass('is-closed')
+          isClosed = true
         } else {   
-          overlay.show();
-          trigger.removeClass('is-closed');
-          trigger.addClass('is-open');
-          isClosed = true;
+          overlay.show()
+          trigger.removeClass('is-closed')
+          trigger.addClass('is-open')
+          isClosed = false
         }
-      }  
+      } 
       $('[data-toggle="offcanvas"]').click(function () {
-            $('#wrapper').toggleClass('toggled');
-      });  
-    });
+            $('#wrapper').toggleClass('toggled')
+      })
+    })
   }
 
   ngOnInit() {
