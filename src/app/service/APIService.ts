865368739cb7c3ee676082e26129640c99a12697
Shwshank <shwshank3@gmail.com>
2019-01-10 14:56:29 +0530

    demo 4

--- src/app/service/APIService.ts
+++ src/app/service/APIService.ts
@@ -24,68 +24,69 @@ export class APIService {
 
   constructor(private http: HttpClient) {}
 
-    loginuser(uname : string, pwd : string){
-      this.ser(uname,pwd)
-      .subscribe(resp => {console.log(resp)})
+  loginuser(uname : string, pwd : string){
+    this.ser(uname,pwd)
+    .subscribe(resp => {console.log(resp)})
+  }
+  ser( uname : string, pwd : string ):Observable<HttpResponse<any>>{
+    let tmp : any = { email : uname, password : pwd }
+    let data = JSON.stringify(tmp)
+    return this.http.post<any>( this.current_URL+'users/login', data )
+    .pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  claimAcc(org_name : string, email : string,pancard : string,gst_number : string,tan_number : string,phone_number : string){
+    let tmp : any = {
+      org_name    : org_name,
+      email       : email,
+      pancard     : pancard,
+      gst_number  : gst_number,
+      tan_number  : tan_number,
+      phone_number: phone_number
     }
-    ser( uname : string, pwd : string ):Observable<HttpResponse<any>>{
-      let tmp : any = { email : uname, password : pwd }
-      let data = JSON.stringify(tmp)
-      return this.http.post<any>( this.current_URL+'users/login', data )
-      .pipe(
-        catchError(this.handleError)
+    let data = JSON.stringify(tmp)
+    return this.http.post<any>( this.current_URL+'', data )
+    .pipe(
+      catchError(this.handleError)
+    )
+    .subscribe(resp => {console.log(resp)})
+  }
+
+
+  sendOTP(email:string, phone_number:string){
+    let tmp : any = {
+      email       : email,
+      phone_number: phone_number
+    }
+    let data = JSON.stringify(tmp)
+    return this.http.post<any>( this.current_URL+'/accounts/get_otp', data )
+  }
+
+  submit_OTP(data:any){
+      let payload = JSON.stringify(data)
+      console.log(payload)
+      return this.http.post<any>( this.current_URL+'/accounts/claim_account', payload )
+  }
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error)
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`,
+        `Backend returned code ${error.message}, ` + `body was: ${error.error}`
       )
-    }
 
-    claimAcc(org_name : string, email : string,pancard : string,gst_number : string,tan_number : string,phone_number : string){
-      let tmp : any = {
-        org_name    : org_name,
-        email       : email,
-        pancard     : pancard,
-        gst_number  : gst_number,
-        tan_number  : tan_number,
-        phone_number: phone_number
-      }
-      let data = JSON.stringify(tmp)
-      return this.http.post<any>( this.current_URL+'', data )
-      .pipe(
-        catchError(this.handleError)
-      )
-      .subscribe(resp => {console.log(resp)})
     }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.')
+  }
 
-
-    sendOTP(email:string, phone_number:string){
-      let tmp : any = {
-        email       : email,
-        phone_number: phone_number
-      }
-      let data = JSON.stringify(tmp)
-      return this.http.post<any>( this.current_URL+'accounts/get_otp', data )
-    }
-
-    submit_OTP(data:any){
-        let payload = JSON.stringify(data)
-        console.log(payload)
-        return this.http.post<any>( this.current_URL+'accounts/claim_account', payload )
-    }
-    handleError(error: HttpErrorResponse) {
-      if (error.error instanceof ErrorEvent) {
-        // A client-side or network error occurred. Handle it accordingly.
-        console.error('An error occurred:', error)
-      } else {
-        // The backend returned an unsuccessful response code.
-        // The response body may contain clues as to what went wrong,
-        console.error(
-          `Backend returned code ${error.status}, ` + `body was: ${error.error}`,
-          `Backend returned code ${error.message}, ` + `body was: ${error.error}`
-        )
-
-      }
-      // return an observable with a user-facing error message
-      return throwError(
-        'Something bad happened; please try again later.')
-    }
 
   setHeader() {
     let token = localStorage.getItem('token');
@@ -97,7 +98,7 @@ export class APIService {
 
   change(data) {
     data = JSON.stringify(data);
-    const request = new HttpRequest('POST', this.current_URL+'users/change_password', data , { reportProgress: true, headers: this.appHeader });
+    const request = new HttpRequest('POST', this.current_URL+'/users/change_password', data , { reportProgress: true, headers: this.appHeader });
     return this.http.request(request)
   }
 
