4363f7748437365b352373b490a55960c209b424
deepak-ranolia <d.ranolia92@gmail.com>
2019-01-29 18:16:24 +0530

    build

--- dist/blockchainUI/main.js
+++ dist/blockchainUI/main.js
@@ -314,6 +314,50 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 module.exports = __webpack_require__(/*! /usr/local/lib/node_modules/v3/blockchain_ui/src/main.ts */"./src/main.ts");
 
 
+/***/ }),
+
+/***/ 1:
+/*!**********************!*\
+  !*** zlib (ignored) ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ 2:
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ 3:
+/*!**********************!*\
+  !*** http (ignored) ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ 4:
+/*!***********************!*\
+  !*** https (ignored) ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
 /***/ })
 
 },[[0,"runtime","vendor"]]]);
--- dist/blockchainUI/polyfills.js
+++ dist/blockchainUI/polyfills.js
@@ -5490,7 +5490,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ 1:
+/***/ 5:
 /*!***************************************************************************************************************************!*\
   !*** multi ./src/polyfills.ts ./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js ***!
   \***************************************************************************************************************************/
@@ -5503,5 +5503,5 @@ module.exports = __webpack_require__(/*! /usr/local/lib/node_modules/v3/blockcha
 
 /***/ })
 
-},[[1,"runtime"]]]);
+},[[5,"runtime"]]]);
 //# sourceMappingURL=polyfills.js.map
